f2ee99b991749974dfaac2bf5718bff43c3ba46a
jnm2 <me@jnm2.com>
2017-02-04 22:03:43 -0500

    Added sample application

---
+++ src/Samples/WinForms/VirtualKeyEvent.cs
@@ -0,0 +1,16 @@
+﻿using LowLevelHooking;
+
+namespace WinForms
+{
+    public struct VirtualKeyEvent
+    {
+        public VirtualKeyEvent(VirtualKey code, bool isUp)
+        {
+            Code = code;
+            IsUp = isUp;
+        }
+
+        public VirtualKey Code { get; }
+        public bool IsUp { get; }
+    }
+}
